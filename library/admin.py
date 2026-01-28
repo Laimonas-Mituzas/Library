efaaeac57278a4b36e13bbd46277d34c84fc639c
Laimonas <laimonas.mituzas@gmail.com>
2026-01-28 13:36:51 +0200

    3 d. paskutnis coomit

--- library/admin.py
+++ library/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'display_books']
+
+
+
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0 # išjungia papildomas tuščias eilutes įvedimui
+    can_delete = False
+    readonly_fields = ['uuid']
+    fields = ['uuid', 'status', 'due_back']
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
+    list_display = ['title', 'author', 'display_genre']
+    inlines = [BookInstanceInLine]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['book', 'status', 'due_back','uuid']
+    list_filter = ['status', 'due_back']
+    list_editable = ['due_back', 'status']
+    search_fields = ['uuid', 'book__title']
+
+    fieldsets = [
+            ('General', {'fields': ('uuid', 'book')}),
+            ('Availability', {'fields': ('status', 'due_back')}),
+        ]
+
 
 
 admin.site.register(Book, BookAdmin)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
--- library/models.py
+++ library/models.py
@@ -5,6 +5,10 @@ import uuid
 class Genre(models.Model):
     name = models.CharField(verbose_name="Name", max_length=200)
 
+    class Meta:
+        verbose_name = "Genre"
+        verbose_name_plural = "Genres"
+
     def __str__(self):
         return self.name
 
@@ -17,13 +21,23 @@ class Author(models.Model):
         verbose_name = "Autorius"
         verbose_name_plural = "Autoriai"
 
+    def display_books(self):
+        return ', '.join(book.title for book in self.books.all())
+
+    display_books.short_description = "Books"
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Book(models.Model):
     title = models.CharField(verbose_name="Title", max_length=200)
-    author = models.ForeignKey(to="Author", verbose_name="Author", on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(to="Author",
+                               verbose_name="Author",
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True,
+                               related_name="books")
     summary = models.TextField(verbose_name="Summary", max_length=1000, help_text="Short Book summary")
     genre = models.ManyToManyField(to='Genre', verbose_name="Genres")
     isbn = models.CharField(verbose_name="ISBN", max_length=13)
@@ -31,14 +45,20 @@ class Book(models.Model):
     def display_genre(self):
         return ", ".join(genre.name for genre in self.genre.all())
 
-    display_genre.short_description = "Genres"
+    display_genre.short_description = "Genre"
 
     def __str__(self):
         return self.title
 
 class BookInstance(models.Model):
-    uuid = models.UUIDField(verbose_name="UUID", default=uuid.uuid4())
-    book = models.ForeignKey(to="Book", verbose_name="Book", on_delete=models.SET_NULL, null=True, blank=True, related_name="instances")
+    uuid = models.UUIDField(verbose_name="ID", default=uuid.uuid4())
+    book = models.ForeignKey(to="Book",
+                             verbose_name="Book",
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             blank=True,
+                             related_name="instances")
+    due_back = models.DateField(verbose_name="Available On", null=True, blank=True)
 
     LOAN_STATUS = (
     ('d', 'Administrated'),
