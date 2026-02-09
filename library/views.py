dc3e3469902c537260615e2204fe59aba5c31399
Laimonas <laimonas.mituzas@gmail.com>
2026-02-09 12:05:29 +0200

    9 d. +Atsiliepimo pateikimo forma is fronto

--- library/views.py
+++ library/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.urls import reverse_lazy
+from django.views.generic.edit import FormMixin
+
 from .models import Book, Author, Genre, BookInstance
 from django.views import generic
 from django.core.paginator import Paginator
 from django.db.models import Q
+from .forms import BookReviewForm
 
 def index(request):
 
@@ -58,10 +61,32 @@ class BookListView(generic.ListView):
     context_object_name = "books"
     paginate_by = 3
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(FormMixin, generic.DetailView):
     model = Book
     template_name = "book.html"
     context_object_name = "book"
+    form_class = BookReviewForm
+
+    # nurodome, kur atsidursime komentaro sėkmės atveju.
+    def get_success_url(self):
+        return reverse("book", kwargs={"pk": self.object.id})
+
+    # standartinis post metodo perrašymas, naudojant FormMixin, galite kopijuoti tiesiai į savo projektą.
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    # štai čia nurodome, kad knyga bus būtent ta, po kuria komentuojame, o vartotojas bus tas, kuris yra prisijungęs.
+    def form_valid(self, form):
+        form.instance.book = self.get_object()
+        form.instance.reviewer = self.request.user
+        form.save()
+        return super().form_valid(form)
+
 
 def search(request):
     query = request.GET.get('query')
